fe203b41f590f7bc55f487dabeb5df5f79504372
Kimbornino <kimbornino@gmail.com>
2018-10-24 19:53:44 -0500

    Modified startup.cs in API.

---
+++ EatLocalAPI/EatLocalAPI/Models/EatLocalContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EatLocalAPI.Models
+{
+    public class EatLocalContext: DbContext
+    {
+        public EatLocalContext(DbContextOptions<EatLocalContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<Markets> Markets { get; set; }
+    }
+}
