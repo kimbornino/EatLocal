8d039c699ab2689bbdcd820b12d6393ebaa181c6
Kimbornino <kimbornino@gmail.com>
2018-10-24 20:00:16 -0500

    Added GET method to API market controller.

--- EatLocalAPI/EatLocalAPI/Controllers/MarketsController.cs
+++ EatLocalAPI/EatLocalAPI/Controllers/MarketsController.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using EatLocalAPI.Models;
 
-namespace EatLocalAPI.Controllers
+namespace TodoApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class MarketsController : ControllerBase
     {
+        private readonly EatLocalContext _context;
+
+        public MarketsController(EatLocalContext context)
+        {
+            _context = context;
+
+            if (_context.Markets.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Markets.Add(new Markets { Name = "Market1" });
+                _context.SaveChanges();
+            }
+        }
     }
 }
